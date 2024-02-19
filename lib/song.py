4f3f1a9a71e8479a1190328ec20a786d05b5c86c
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-19 14:29:30 +1100

    finsihed tests

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,51 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+    
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.add(genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.add(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+    
+    @classmethod
+    def get_genre_count(cls):
+        return cls.genre_count
+    
+    @classmethod
+    def get_artist_count(cls):
+        return cls.artist_count
